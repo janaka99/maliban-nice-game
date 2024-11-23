66256c0d653b643c7c9332f0e4bd55ee711a4a2e
janaka99 <janakachamith88@gmail.com>
2024-11-23 19:53:19 +0530

    pixel code removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,35 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1560558655341594');
-        fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{
-              display: " none",
-            }}
-            src="https://www.facebook.com/tr?id=1560558655341594&ev=PageView&noscript=1"
-          />
-        </noscript>
-      </head>
+      <head></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-[100svh] w-screen overflow-x-hidden`}
         style={{
